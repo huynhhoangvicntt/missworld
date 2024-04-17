79007f0f29185f002c377adc3aabd65492c0f339
Vi Huynh <hoangvicntt2k@gmail.com>
2024-04-17 16:13:56 +0700

    add folder module

--- modules/missworld/action_mysql.php
+++ modules/missworld/action_mysql.php
@@ -10,10 +10,10 @@
  */
 
 if (!defined('NV_IS_FILE_MODULES')) {
-  die('Stop!!!');
+    exit('Stop!!!');
 }
 
-$sql_drop_module = array();
+$sql_drop_module = [];
 
 $sql_drop_module[] = "DROP TABLE IF EXISTS " . $db_config['prefix'] . "_" . $lang . "_" . $module_data . "_player;";
 $sql_drop_module[] = "DROP TABLE IF EXISTS " . $db_config['prefix'] . "_" . $lang . "_" . $module_data . "_voter;";
@@ -21,26 +21,28 @@ $sql_drop_module[] = "DROP TABLE IF EXISTS " . $db_config['prefix'] . "_" . $lan
 $sql_create_module = $sql_drop_module;
 
 $sql_create_module[] = "CREATE TABLE IF NOT EXISTS " . $db_config['prefix'] . "_" . $lang . "_" . $module_data . "_player (
-id int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+id int(11) unsigned NOT NULL AUTO_INCREMENT,
 name varchar(190) NOT NULL COMMENT 'Họ và tên thí sinh',
 alias varchar(190) NOT NULL COMMENT 'Liên kết tĩnh không trùng',
 dob_address varchar(190) NOT NULL COMMENT 'Ngày sinh, nơi sinh',
-height int(11) NOT NULL DEFAULT 0 COMMENT 'Chiều cao',
+height int(11) NOT NULL DEFAULT '0' COMMENT 'Chiều cao',
 measurements varchar(190) NOT NULL COMMENT 'Số đo ba vòng',
 email varchar(190) NOT NULL COMMENT 'Địa chỉ email',
 image varchar(255) NOT NULL COMMENT 'Ảnh hồ sơ',
-weight int(10) NOT NULL DEFAULT 1,
-vote_count int(11) NOT NULL DEFAULT 0 COMMENT 'Số lượt vote',
+weight smallint(4) unsigned NOT NULL DEFAULT '0',
+add_time int(11) NOT NULL DEFAULT '0',
+edit_time int(11) NOT NULL DEFAULT '0',
+vote_count int(11) NOT NULL DEFAULT '0' COMMENT 'Số lượt vote',
 PRIMARY KEY (id),
-KEY alias (alias)
+UNIQUE KEY alias (alias)
 ) ENGINE=InnoDB";
 
 $sql_create_module[] = "CREATE TABLE IF NOT EXISTS " . $db_config['prefix'] . "_" . $lang . "_" . $module_data . "_voter (
 id int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
-userid int(11) UNSIGNED NOT NULL DEFAULT 0 COMMENT 'Id người dùng có tài khoản',
+userid int(11) UNSIGNED NOT NULL DEFAULT '0' COMMENT 'Id người dùng có tài khoản',
 name varchar(190) NOT NULL COMMENT 'Họ tên người vote',
 email varchar(190) NOT NULL COMMENT 'Email người vote',
-vote_time int(11) UNSIGNED NOT NULL DEFAULT 0,
+vote_time int(11) unsigned NOT NULL DEFAULT '0',
 PRIMARY KEY (id),
 UNIQUE KEY email (email),
 KEY userid (userid)
